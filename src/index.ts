539988dc744a308e3efb0035ac32460007e3938d
th-release <th.release.cth@gmail.com>
2022-12-20 09:34:25 +0900

    `target` => `ES2017`

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import fs from 'fs';
 import textToImage from 'text-to-image';
 
-textToImage.generate('text here!', {
-  maxWidth: 720,
-  fontSize: 18,
-  fontFamily: 'Arial',
-  lineHeight: 30,
-  margin: 5,
-  bgColor: "white",
-  textColor: "black"
-}).then(function (dataUri) {
-  const buffer = Buffer.from(dataUri, "base64");
-  fs.writeFileSync("image.jpg", buffer);
-})
+async function gen(text: string) {
+  await textToImage.generate(text, {
+    debug: true,
+    maxWidth: 720,
+    fontSize: 18,
+    fontFamily: 'Arial',
+    lineHeight: 30,
+    margin: 5,
+    bgColor: "white",
+    textColor: "black"
+  }).then(function (dataUri) {
+    const buffer = Buffer.from(dataUri, "base64");
+    fs.writeFileSync("image.jpg", buffer);
+  })
+}
+
+gen("text here!")
